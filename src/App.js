da014e8d294d91497999e68130857cd0a9c3a77e
VidraTo <39879137+TommyVidra@users.noreply.github.com>
2024-02-25 16:30:04 +0100

    lol

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>ŽENIM SE</h2>
+        <iframe src="https://www.youtube.com/embed/aqAlmzyzURQ?si=Xyp1NtMojeZE4670"  autoplay></iframe>
+        {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/aqAlmzyzURQ?si=Xyp1NtMojeZE4670" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> */}
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
